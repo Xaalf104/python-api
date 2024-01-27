1cacdf98e8f5d6c0ca49a78a913b5c72adb278d4
Xaalf104 <alsharief.dinglasa06@gmail.com>
2024-01-27 10:05:47 +0800

    lists added

--- farmerview.py
+++ farmerview.py
@@ -1,45 +1,67 @@
 import pandas as pd
 
+# report lists
+report_wc = []
+report_cy = []
+report_ny = []
+
+# strings
+terrible_wc = "Withered crops are too high"
+bad_wc = "Withered crops are at a concerning level"
+good_wc = "Withered crops are within acceptable limits"
+
+excellent_cy = "Crop yield is commendable"
+bad_cy = "Crop yield is low"
+average_cy = "Crop yield is average"
+terrible_cy = "Crop yield is terrible"
+
+excellent_ny = "Net yield is commendable"
+bad_ny = "Net yield is average"
+
 
 def generate_individual_reports(dataframe):
     individual_reports = []
 
-    report = f"Report for Crops:\n"
     for index, row in dataframe.iterrows():
-        report += "\n"
+        report = f"Report for {row['plant']} crop:\n"
+
         # Analyze withered crops
         if row["withered_crops"] > 5:
-            report += f"For {row['plant']} crop, withered crops are too high,"
+            report += f"Withered crops are too high, "
+            report_wc.append(terrible_wc)
         elif row["withered_crops"] >= 3:
-            report += (
-                f"For {row['plant']} crop, withered crops are at a concerning level,"
-            )
+            report += f"Withered crops are at a concerning level, "
+            report_wc.append(bad_wc)
         else:
-            report += (
-                f"For {row['plant']} crop, withered crops are within acceptable limits,"
-            )
+            report += f"Withered crops are within acceptable limits, "
+            report_wc.append(good_wc)
 
         # Analyze crop yield
-        if row["crop_yield"] >= 6:
-            report += f"crop yield is commendable, "
-        elif row["crop_yield"] < 3:
-            report += f"crop yield is low, "
-        else:
+        if row["crop_yield"] >= 5:
             report += f"crop yield is average, "
+            report_cy.append(report)
+        elif row["crop_yield"] < 5:
+            report += f"crop yield is low, "
+            report_cy.append(report)
+        elif row["crop_yield"] < 0:
+            report += f"crop yield is terrible, "
+            report_cy.append(report)
+        elif row["crop_yield"] >= 10:
+            report += f"crop yield is commendable, "
+            report_cy.append(report)
 
         # Analyze net yield
-        if row["net_yield"] >= 10:
+        if row["net_yield"] >= 12:
             report += f"net yield is commendable.\n"
-        elif row["net_yield"] <= 8:
-            report += f"net yield is below expectations. "
-        else:
-            report += f"net yield is average.\n"
+            report_ny.append(report)
+        elif row["net_yield"] >= 8:
+            report += f"net yield is average. \n"
+            report_ny.append(report)
+        elif row["net_yield"] < 8:
+            report += f"net yield is below expectations\n"
+            report_ny.append(report)
 
-        # apped reports
-        report += "\nTags: \n\n"
-        individual_reports.append(report)
-
-    return individual_reports
+    return report_wc, report_cy, report_ny
 
 
 if __name__ == "__main__":
@@ -48,9 +70,17 @@ if __name__ == "__main__":
     # Read Excel file
     df = pd.read_excel(excel_file_path)
 
-    # Generate individual reports for each row
-    reports = generate_individual_reports(df)
+    # generate reports
+    reports_wc, reports_cy, reports_ny = generate_individual_reports(df)
 
-    # Print individual reports for each crop rport
-    for report in reports:
+    print("Reports for Withered Crops:")
+    for report in reports_wc:
+        print(report)
+
+    print("\nReports for Crop Yield:")
+    for report in reports_cy:
+        print(report)
+
+    print("\nReports for Net Yield:")
+    for report in reports_ny:
         print(report)
